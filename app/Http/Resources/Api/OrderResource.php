e3eadf3237312d27b3d4beb409f58539cb80b426
unknown <anddhital@gmail.com>
2024-04-18 12:44:07 +0545

    Updated API

--- app/Http/Resources/Api/OrderResource.php
+++ app/Http/Resources/Api/OrderResource.php
@@ -17,7 +17,7 @@ class OrderResource extends JsonResource
         {
             return [
                 'id' => $this->id,
-                'user_id' => $this->user_id,
+                'user_id' =>(int)$this->user_id,
                 'total_price' => $this->total_price,
                 'created_at' => $this->created_at,
                 'updated_at' => $this->updated_at,
